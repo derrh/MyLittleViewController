259c885dc39ce0f0cec341cd1ff70bf3f4084a33
Derrick J. Hathaway <derrick@instructure.com>
2013-10-01 15:49:16 -0600

    getting the project ready for ci

---
+++ MyLittleViewControllerTestBundle/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MyLittleViewControllerTestBundle
+//
+//  Created by derrick on 10/1/13.
+//  Copyright (c) 2013 Instructure. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MyLittleViewControllerTestBundle/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MyLittleViewControllerTestBundle
+//
+//  Created by derrick on 10/1/13.
+//  Copyright (c) 2013 Instructure. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
