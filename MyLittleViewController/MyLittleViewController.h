1785fa8237e5ade54b6cb7545c7a91cc10c628be
Derrick J. Hathaway <derrick@instructure.com>
2013-10-01 15:28:30 -0600

    initial project setup.

---
+++ MyLittleViewController/MyLittleViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyLittleViewController.h
+//  MyLittleViewController
+//
+//  Created by derrick on 10/1/13.
+//  Copyright (c) 2013 Instructure. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyLittleViewController : NSObject
+
+@end
---
+++ MyLittleViewController/MyLittleViewController.m
@@ -0,0 +1,13 @@
+//
+//  MyLittleViewController.m
+//  MyLittleViewController
+//
+//  Created by derrick on 10/1/13.
+//  Copyright (c) 2013 Instructure. All rights reserved.
+//
+
+#import "MyLittleViewController.h"
+
+@implementation MyLittleViewController
+
+@end
